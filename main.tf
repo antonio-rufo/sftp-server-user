c422cfddc653e0cb9711492aca1716758614278a
Antonio Cuenco <antonio@Antonios-MacBook-Air.local>
2023-10-08 21:54:29 +1000

    feat: adding in User2 in SFTP Server 1

--- main.tf
+++ main.tf
@@ -134,6 +134,54 @@ module "sftp_user_module_admin" {
   home_directory_key_prefix = ""
 }
 
+### SAMPLE USER WITH ADDITIONAL POLICY FROM USER1 - Just incase you want user2 to have access to User1 data
+module "sftp_user_module_user2" {
+  source          = "./transfer_sftp_user_module/"
+  sftp_server_id  = module.sftp_server_module.sftp_server_id
+  ssh_public_keys = [file("./ssh_keys/user1_sshkey")]
+  user_name       = "user2"
+  role_name       = "user2-sftp-role"
+  home_directory_bucket = {
+    arn = module.s3_bucket.bucket_arn
+    id  = module.s3_bucket.bucket_id
+  }
+  home_directory_key_prefix = "user2/"
+  iam_role_additional_policies = {
+    additional = aws_iam_policy.additional.arn
+  }
+}
+
+resource "aws_iam_policy" "additional" {
+  name = "add-user1-permissions-to-user2"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObjectACL",
+          "s3:PutObject",
+          "s3:GetObjectVersion",
+          "s3:GetObjectACL",
+          "s3:GetObject",
+          "s3:DeleteObjectVersion",
+          "s3:DeleteObject"
+        ]
+        Effect   = "Allow",
+        Resource = "arn:aws:s3:::sftp-middle-office-antonio/user1/*"
+      },
+      {
+        Action = [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ]
+        Effect   = "Allow",
+        Resource = "arn:aws:s3:::sftp-middle-office-antonio"
+      }
+    ]
+  })
+}
+
 ###############################################################################
 # SFTP Server 2
 ###############################################################################
