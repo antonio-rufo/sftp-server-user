1283dcb85073b5b494c5093e0b94e6057b517bc6
Antonio Cuenco <antonio@Antonios-MacBook-Air.local>
2023-10-08 21:45:52 +1000

    feat: adding in SFTP Server 2

--- main.tf
+++ main.tf
@@ -133,3 +133,19 @@ module "sftp_user_module_admin" {
   }
   home_directory_key_prefix = ""
 }
+
+###############################################################################
+# SFTP Server 2
+###############################################################################
+module "sftp_server_module_2" {
+  source                 = "./transfer_sftp_server_module/"
+  name                   = "sftp_test.sftp_server_2"
+  iam_role_name          = "sftp-logging-role-2"
+  vpc_id                 = module.vpc.vpc_id
+  subnet_ids             = module.vpc.private_subnets
+  vpc_security_group_ids = [module.security_group_ec2.security_group_id]
+
+  tags = {
+    Environment = var.environment
+  }
+}
