e20b833d589b348eaacac1681f33660c02eb3fb4
Antonio Cuenco <antonio.cuenco@auspost.com.au>
2023-11-10 14:29:45 +1000

    feat: add-on

--- main.tf
+++ main.tf
@@ -24,29 +24,29 @@ terraform {
 ###############################################################################
 data "aws_availability_zones" "available" {}
 
-###############################################################################
-# Sample VPC
-###############################################################################
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  # Single NAT gateway
-
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
-
-  tags = {
-    Environment = var.environment
-  }
-}
+# ###############################################################################
+# # Sample VPC
+# ###############################################################################
+# module "vpc" {
+#   source = "terraform-aws-modules/vpc/aws"
+#
+#   name = "my-vpc"
+#   cidr = "10.0.0.0/16"
+#
+#   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#
+#   # Single NAT gateway
+#
+#   enable_nat_gateway     = true
+#   single_nat_gateway     = true
+#   one_nat_gateway_per_az = false
+#
+#   tags = {
+#     Environment = var.environment
+#   }
+# }
 
 ###############################################################################
 # S3
@@ -58,29 +58,29 @@ module "s3_bucket" {
   app_name       = "middle-office-sftp-server-antonio"
 }
 
-###############################################################################
-# Security Group
-###############################################################################
-module "security_group_ec2" {
-  source = "./security_group_module/"
-
-  name        = "test-sftp-server-sg"
-  description = "Security group for SFTP VPC"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      description = "Allow connections from everywhere on port 22"
-      cidr_blocks = "0.0.0.0/0"
-    }
-  ]
-
-  egress_rules = ["all-all"]
-
-  tags = {
-    Environment = var.environment
-  }
-}
+# ###############################################################################
+# # Security Group
+# ###############################################################################
+# module "security_group_ec2" {
+#   source = "./security_group_module/"
+#
+#   name        = "test-sftp-server-sg"
+#   description = "Security group for SFTP VPC"
+#   vpc_id      = module.vpc.vpc_id
+#
+#   ingress_with_cidr_blocks = [
+#     {
+#       from_port   = 22
+#       to_port     = 22
+#       protocol    = "tcp"
+#       description = "Allow connections from everywhere on port 22"
+#       cidr_blocks = "0.0.0.0/0"
+#     }
+#   ]
+#
+#   egress_rules = ["all-all"]
+#
+#   tags = {
+#     Environment = var.environment
+#   }
+# }
